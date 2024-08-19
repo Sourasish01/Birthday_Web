402ad369dd749cbbcf0d22c86811ce9819efa4be
Sourasish01 <156464064+Sourasish01@users.noreply.github.com>
2024-08-19 17:11:55 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,73 @@
+
+const balloonContainer = document.getElementById("balloon-container");
+function random(num) {
+  return Math.floor(Math.random() * num);
+}
+
+function getRandomStyles() {
+  var r = random(255);
+  var g = random(255);
+  var b = random(255);
+  var mt = random(200);
+  var ml = random(50);
+  var dur = random(5) + 5;
+  return `
+  background-color: rgba(${r},${g},${b},0.7);
+  color: rgba(${r},${g},${b},0.7); 
+  box-shadow: inset -7px -3px 10px rgba(${r - 10},${g - 10},${b - 10},0.7);
+  margin: ${mt}px 0 0 ${ml}px;
+  animation: float ${dur}s ease-in infinite
+  `;
+}
+
+function createBalloons(num) {
+  for (var i = num; i > 0; i--) {
+    var balloon = document.createElement("div");
+    balloon.className = "balloon";
+    balloon.style.cssText = getRandomStyles();
+    balloonContainer.append(balloon);
+  }
+}
+
+window.addEventListener("load", () => {
+  createBalloons(30)
+});
+
+
+
+
+
+document.getElementById('confetti').onclick =function(){
+  var duration = 10 * 1000;
+  var animationEnd = Date.now() + duration;
+  var defaults = { startVelocity: 50, spread: 360, ticks: 60, zIndex: 0 };
+  
+  function randomInRange(min, max) {
+    return Math.random() * (max - min) + min;
+  }
+  
+  var interval = setInterval(function() {
+
+    var timeLeft = animationEnd - Date.now();
+  
+    if (timeLeft <= 0) {
+      return clearInterval(interval);
+    }
+  
+    var particleCount = 500 * (timeLeft / duration);
+    // since particles fall down, start a bit higher than random
+    confetti({ ...defaults, particleCount, origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 } });
+    confetti({ ...defaults, particleCount, origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 } });
+  }, 250);
+
+}
+
+let menu =document.getElementById('menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = function(){
+  navbar.classList.toggle('active');
+}
+
+
+
